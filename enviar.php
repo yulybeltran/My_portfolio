ec8297a382a523f6b30c3aa78629efa0091ed7f3
Marcela_Beltran <yulybeltran082@gmail.com>
2021-12-18 14:33:47 -0500

    Canbios en php

--- enviar.php
+++ enviar.php
@@ -1,24 +1,26 @@
 <?php
 $nombre = $_POST['nombre'];
 $correo = $_POST['correo'];
-$ctex = $_POST['mensaje'];
+$mensaje = $_POST['mensaje'];
 
-$header = 'From:' . $correo . "\r\n";
+
+
+$header = 'From:' . $correo. "\r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . "\r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
-$mensaje = "Este mensaje fue enviado por: " . $nombre . "/r/n";
-$mensaje .= "Su e-mail es" . $correo . "\r\n";
-$mensaje .= "Mensaje: " . $_POST['ctex'] .  "\r\n";
-$ctex .=" Enviado el : " . date('d/m/Y' , time());
+$mensaje = "Este mensaje fue enviado por: " . $nombre . "\r\n";
+$mensaje .= "Su e-mail es" . $correo. "\r\n";
+$mensaje .= "Mensaje: " . $_POST['mensaje'] .  "\r\n";
+$mensaje .=" Enviado el : " . date('d/m/Y' , time());
 
-$para ='yulybeltran082@gmail.com';
-$asunto = 'Asunto del mensaje';
+$para = 'yulybeltran082@gmail.com';
+$asunto = 'Mensaje Portafolio';
 
 
-mail($para, $asunto, utf8_decode($ctex), $header);
+mail($para, $asunto, utf8_decode($mensaje), $header);
  
 
-header("Location:index.html");
+$header("<Location:index.html>");
 ?>
