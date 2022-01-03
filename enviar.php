6997b8a970b341330304112408b20454a55d3c34
Marcela_Beltran <yulybeltran082@gmail.com>
2022-01-03 15:50:22 -0500

    contact form settings

--- enviar.php
+++ enviar.php
@@ -3,15 +3,13 @@ $nombre = $_POST['nombre'];
 $correo = $_POST['correo'];
 $mensaje = $_POST['mensaje'];
 
-
-
 $header = 'From:' . $correo. "\r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . "\r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
 $mensaje = "Este mensaje fue enviado por: " . $nombre . "\r\n";
-$mensaje .= "Su e-mail es" . $correo. "\r\n";
+$mensaje .= "e-mail " . $correo. "\r\n";
 $mensaje .= "Mensaje: " . $_POST['mensaje'] .  "\r\n";
 $mensaje .=" Enviado el : " . date('d/m/Y' , time());
 
@@ -22,5 +20,5 @@ $asunto = 'Mensaje Portafolio';
 mail($para, $asunto, utf8_decode($mensaje), $header);
  
 
-$header("<Location:index.html>");
+header("location:index.html");
 ?>
